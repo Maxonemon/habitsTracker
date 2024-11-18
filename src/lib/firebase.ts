9f6ca3830e0f9e2a344a7a791352958484498d65
Maxonemon <110642351+Maxonemon@users.noreply.github.com>
2024-11-18 16:19:40 +0000

    update auth

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,15 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDWNzQ7vTdtoJ571MPunoC-fdr7LghNS3o",
-  authDomain: "habittracks-23a01.firebaseapp.com",
-  projectId: "habittracks-23a01",
-  storageBucket: "habittracks-23a01.firebasestorage.app",
-  messagingSenderId: "533339425992",
-  appId: "1:533339425992:web:5edcdb39867831724a69fd",
+  apiKey: "AIzaSyCCOUX8QNaJZ4CovoemJ9u6mpK4dfnF9IA",
+  authDomain: "test-435913.firebaseapp.com",
+  projectId: "test-435913",
+  storageBucket: "test-435913.firebasestorage.app",
+  messagingSenderId: "833132884665",
+  appId: "1:833132884665:web:130bbafa5b44e52be1a168",
+  measurementId: "G-0NS0B2MQG7",
 };
+
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
